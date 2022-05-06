a1894b86fd5b5868f8346507aa4fa08afd7a8e7b
Seonmi Jung <devkatej927@gmail.com>
2022-05-06 12:22:39 +0900

    feat: Calendar에서 tbody부분을 3type(year/month/date 선택)으로 재활용하기 위한 작업 중

--- src/components/BtnCalendar/index.js
+++ src/components/BtnCalendar/index.js
@@ -1,53 +1,81 @@
+import { useState } from 'react'
 import styles from './UpdateTodo.module.scss'
 import * as dayjs from 'dayjs'
 import * as localeData from 'dayjs/plugin/localeData'
 import 'dayjs/locale/bs'
 
 export default function BtnCalendar() {
+  const [selected, setSelected] = useState({ year: dayjs().year(), month: dayjs().month(), date: dayjs().date() })
+  const [navi, setNavi] = useState([dayjs().format('MMMM'), dayjs().year()])
+
+  const { year, month, date } = selected
+  let selectedDay = dayjs(`${year}-${month}-${date}`)
+
   dayjs.extend(localeData)
-  const dateOfToday = dayjs().date()
-  const weekdaysMin = dayjs().locale('en-us').localeData().weekdaysMin()
+
+  const MONTHS = dayjs.monthsShort()
+  const WEEKDAY = dayjs.weekdaysMin()
+
   const firstDayOfMonth = dayjs().startOf('month').day()
   const daysInMonth = dayjs().daysInMonth()
   const blanksInMonth = Array(firstDayOfMonth).fill('')
   const spreadDaysInMonth = Array.from({ length: daysInMonth }, (v, i) => i + 1)
   const shownDaysInMonth = [...blanksInMonth, ...spreadDaysInMonth]
 
-  let weekIndex = -1
-  const month = shownDaysInMonth.reduce((acc, cur, i) => {
+  let rowIdx = -1
+  const manufacturedTbodyData = shownDaysInMonth.reduce((acc, cur, i) => {
     if (!(i % 7)) {
       acc.push([cur])
-      weekIndex += 1
+      rowIdx += 1
     } else {
-      acc[weekIndex].push(cur)
+      acc[rowIdx].push(cur)
     }
     return acc
   }, [])
 
-  console.log(dayjs().date())
+  console.log(
+    dayjs(),
+    'month',
+    MONTHS,
+    'selectedDay',
+    selectedDay,
+    'firstDayOfMonth',
+    dayjs('2021-1-02').startOf('month').day(),
+    'daysInMonth',
+    dayjs('2021-2-02').daysInMonth()
+  )
 
   return (
-    <table className={styles.btnCalendar}>
-      <thead>
-        <tr>
-          {weekdaysMin.map((day) => (
-            <th key={day} className={styles.weekDay}>
-              {day}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {month.map((week, idx) => (
-          <tr key={`week-${idx + 1}`}>
-            {week.map((date, index) => (
-              <td key={`date-${index + 1}`} className={`date ${dateOfToday === date && 'today'}`}>
-                {date}
-              </td>
+    <section>
+      <div>
+        {navi.map((el) => (
+          <button key={el} type='button'>
+            {el}
+          </button>
+        ))}
+      </div>
+      <table className={styles.btnCalendar}>
+        <thead>
+          <tr>
+            {WEEKDAY.map((day) => (
+              <th key={day} className={styles.weekDay}>
+                {day}
+              </th>
             ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {manufacturedTbodyData.map((eachRow, idx) => (
+            <tr key={`week-${idx + 1}`}>
+              {eachRow.map((el, index) => (
+                <td key={`date-${index + 1}`} className={`date ${date === el && 'selected'}`}>
+                  {el}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </section>
   )
 }
